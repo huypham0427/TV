dc34cf01073e7b513fa12e26fdd9313a75c74c70
huypham0427 <ptanhhuy27@yahoo.com>
2023-09-06 12:15:14 -0700

    3000

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 // Configure Multer to handle file uploads
 const storage = multer.diskStorage({
